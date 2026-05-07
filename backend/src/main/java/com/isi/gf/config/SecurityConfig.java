81ead53311d57ab9012533b6f6ad1d4efd49ed5b
Wiem <wiem@example.com>
2026-05-07 16:55:04 +0100

    Update config imports

--- backend/src/main/java/com/isi/gf/config/SecurityConfig.java
+++ backend/src/main/java/com/isi/gf/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.isi.gf.config;
 
-import com.isi.gf.security.JwtAuthFilter;
+import com.isi.gf.config.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
